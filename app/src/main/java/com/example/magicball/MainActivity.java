6704c0424f147478e3d8db5e6fc4ba3258e7a4c2
PedroSagredoF <pedro.sagredof@gmail.com>
2021-05-16 21:55:36 -0400

    Se agrega funcion de mensaje aleatorio

--- app/src/main/java/com/example/magicball/MainActivity.java
+++ app/src/main/java/com/example/magicball/MainActivity.java
@@ -1,14 +1,44 @@
 package com.example.magicball;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+import java.util.Random;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private ImageButton ball;
+    private TextView respuesta;
+    private String[] respuestaArray ={"Puede Ser","Pero Por Supuesto","No lo se","Pregunta de Nuevo","50 y 50","Claro que si",
+                                        "Definitivamente puede no ser", "NO", "Si", "Estoy ocupado, pregunta mas tarde", "Dudoso"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Referencia la Imagen
+        ball = findViewById(R.id.btn);
+
+        //Referencia al Texto
+        respuesta = findViewById(R.id.respuestaVista);
+
+        ball.setOnClickListener(this);
+        Toast.makeText(MainActivity.this, "Consulta tu destino", Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    public void onClick(View v){
+        //Creación Metotodo Switch
+        switch (v.getId()){
+            case R.id.btn:
+                int rand = new Random().nextInt(respuestaArray.length);
+                respuesta.setText(respuestaArray[rand]);
+                break;
+        }
     }
 }
